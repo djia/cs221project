9a3bb24872c0d9b0a8f5fcdec26c2add0293de1f
Kannu Mehta <kannumehta@gmail.com>
2013-08-12 22:01:00 -0700

    Complying with Matlab's format

--- fmri_project/drtoolbox/runPCA.m
+++ fmri_project/drtoolbox/runPCA.m
@@ -6,12 +6,12 @@ function [examplesTraining, examplesTest] = runPCA(examplesTraining, examplesTes
 examplesTest = mapping.U' * (examplesTest' - repmat(mapping.mean, 1, size(examplesTest', 2)));
 
 if (bPCAWhitening)
-    examplesTraining = examplesTraining./mapping.lambda;
-    examplesTest = examplesTest./mapping.lambda;
+    examplesTraining = bsxfun(@rdivide, examplesTraining,mapping.lambda);
+    examplesTest = bsxfun(@rdivide, examplesTest,mapping.lambda);
 end
 if (bZCAWhitening)
-    examplesTraining = mapping.U*(examplesTraining./mapping.lambda);
-    examplesTest = mapping.U*(examplesTest./mapping.lambda);
+    examplesTraining = mapping.U*bsxfun(@rdivide, examplesTraining, mapping.lambda);
+    examplesTest = mapping.U*bsxfun(@rdivide, examplesTest, mapping.lambda);
 end
 examplesTraining = examplesTraining';
 examplesTest = examplesTest';
